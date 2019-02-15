90bb8c02ea4b951181609d8547c7759eba459ca0
VietAnhLee <30468082+VietAnhLee@users.noreply.github.com>
2019-02-15 16:39:48 +0700

    Add files via upload

---
+++ SimpleCalculator/aMode.cpp
@@ -0,0 +1,21 @@
+#include "aMode.h"
+
+int add(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int sub(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int divi(int num1, int num2)
+{
+    return num1/num2;
+}
+
+int mul(int num1, int num2)
+{
+    return num1*num2;
+}
